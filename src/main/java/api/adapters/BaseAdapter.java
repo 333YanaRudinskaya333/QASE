e7274dcf9de768ffb47808ac0f7b54f59981ff82
Yana_Rudinskaya <rudinskaya.yana@gmail.com>
2026-07-18 23:47:37 +0300

    property reader

--- src/main/java/api/adapters/BaseAdapter.java
+++ src/main/java/api/adapters/BaseAdapter.java
@@ -15,7 +15,9 @@ import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 public class BaseAdapter {
-    public static final String TOKEN = PropertyReader.getProperty("token");
+    public static final String TOKEN = System.getProperty("token") != null
+            ? System.getProperty("token")
+            : PropertyReader.getProperty("token");
     public static Gson GSON = new GsonBuilder()
             .excludeFieldsWithoutExposeAnnotation()
             .setPrettyPrinting()
